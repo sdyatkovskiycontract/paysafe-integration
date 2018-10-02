00c9515e905de5aa05c8ff95b617f9a908ec9dce
Stepan Dyatkovskiy <contract@dyatkovskiy.com>
2018-10-02 13:06:40 +0300

    Tests: added TestNG sketch.

---
+++ src/test/java/com/magenta/sc/paysafe/PaysafeIntegrationTests.java
@@ -0,0 +1,25 @@
+package com.magenta.sc.paysafe;
+import org.testng.annotations.*;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class PaysafeIntegrationTests {
+
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+    }
+
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
+
+}
