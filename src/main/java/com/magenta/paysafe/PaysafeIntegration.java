8fa52ab445b6f84d86e948d79a1ba621d0e48bea
Stepan Dyatkovskiy <contract@dyatkovskiy.com>
2018-10-02 11:45:35 +0300

    PaysafeIntegration: initial commit.

---
+++ src/main/java/com/magenta/paysafe/PaysafeIntegration.java
@@ -0,0 +1,13 @@
+package com.magenta.paysafe;
+
+/**
+ * Hello world!
+ *
+ */
+public class PaysafeIntegration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
