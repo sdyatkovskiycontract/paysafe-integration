a929692f5f50c36be8501f5c8790a3eb4ddf9140
Stepan Dyatkovskiy <contract@dyatkovskiy.com>
2018-10-04 14:22:26 +0300

    Added mock entity manager factory.

--- src/test/java/com/magenta/sc/paysafe/mock/MockCardInfo.java
+++ src/test/java/com/magenta/sc/paysafe/mock/MockCardInfo.java
@@ -2,6 +2,7 @@ package com.magenta.sc.paysafe.mock;
 
 public class MockCardInfo {
     private Long companyId;
+    private String profileId;
 
     public Long getCompanyId() {
         return companyId;
@@ -9,5 +10,13 @@ public class MockCardInfo {
 
     public void setCompanyId(Long companyId) {
         this.companyId = companyId;
+    }
+
+    public String getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(String profileId) {
+        this.profileId = profileId;
     }
 }
