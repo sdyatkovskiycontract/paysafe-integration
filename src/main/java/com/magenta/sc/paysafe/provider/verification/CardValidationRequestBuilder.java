d50060731caf8799f4c2a37c66f471f4af638c33
Stepan Dyatkovskiy <contract@dyatkovskiy.com>
2018-10-04 19:04:48 +0300

    Added card verification builder (as separate class).

---
+++ src/main/java/com/magenta/sc/paysafe/provider/verification/CardValidationRequestBuilder.java
@@ -0,0 +1,44 @@
+package com.magenta.sc.paysafe.provider.verification;
+
+import com.magenta.sc.core.entity.customer.CreditCard;
+import com.magenta.sc.exception.CreditCardException;
+import com.magenta.sc.paysafe.helpers.HolderNameInfo;
+import com.paysafe.cardpayments.Card;
+import com.paysafe.cardpayments.Verification;
+
+public class ValidationRequestBuilder {
+
+    public static Verification build(CardValidationParameters parameters) throws CreditCardException {
+
+        CreditCard card = parameters.getCard();
+        String merchantRefNum = parameters.getMerchantRefNum();
+
+        String holderName = card.getHolderName();
+
+        HolderNameInfo holder = HolderNameInfo.fromString(holderName);
+
+        Verification.VerificationBuilder builder = Verification.builder();
+        Card.CardBuilder<Verification.VerificationBuilder> cardBuilder = builder.card();
+
+        cardBuilder.cardNum(card.getNumber())
+            .cardExpiry()
+                .month(card.getExpireDate().getMonthOfYear())
+                .year(card.getExpireDate().getYearOfEra())
+            .done();
+
+        if (parameters.isCheckCvv())
+            cardBuilder.cvv(card.getSecureCode());
+
+        builder
+            .merchantRefNum(merchantRefNum)
+            .profile()
+                .firstName(holder.getFirstName())
+                .lastName(holder.getLastName())
+            .done()
+            .billingDetails()
+                .zip(card.getPostcode())
+            .done();
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/com/magenta/sc/paysafe/provider/verification/CardVerificationParameters.java
@@ -0,0 +1,33 @@
+package com.magenta.sc.paysafe.provider.verification;
+
+import com.magenta.sc.core.entity.customer.CreditCard;
+
+public class CardValidationParameters {
+    private final CreditCard card;
+    private final String merchantRefNum;
+    private final boolean checkCvv;
+    private final boolean checkPostcode;
+
+    public CardValidationParameters(CreditCard card, String merchantRefNum, boolean checkCvv, boolean checkPostcode) {
+        this.card = card;
+        this.merchantRefNum = merchantRefNum;
+        this.checkCvv = checkCvv;
+        this.checkPostcode = checkPostcode;
+    }
+
+    public CreditCard getCard() {
+        return card;
+    }
+
+    public String getMerchantRefNum() {
+        return merchantRefNum;
+    }
+
+    public boolean isCheckCvv() {
+        return checkCvv;
+    }
+
+    public boolean isCheckPostcode() {
+        return checkPostcode;
+    }
+}
