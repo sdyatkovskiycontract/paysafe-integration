6403bc1fab53878249aef332424dde3106045341
Stepan Dyatkovskiy <contract@dyatkovskiy.com>
2018-10-04 19:41:56 +0300

    Added card token utils.

---
+++ src/main/java/com/magenta/sc/paysafe/helpers/CardTokenUtils.java
@@ -0,0 +1,27 @@
+package com.magenta.sc.paysafe.helpers;
+
+public class CardTokenParser {
+
+    private static final String SEPARATOR = ";";
+    private static int PROFILE_ID_IDX = 0;
+    private static int PAYMENT_TOKEN_IDX = 1;
+
+    public static String buildCardToken(String profileId, String paymentToken) {
+        return String.format("%s%s%s", profileId, SEPARATOR, paymentToken);
+    }
+
+    public static String getProfileId(String token) {
+        String[] parts = token.split(SEPARATOR);
+        if (parts.length != 2)
+            throw new IllegalArgumentException("Invalid credit card token");
+        return parts[PROFILE_ID_IDX];
+    }
+
+    public static String getPaymentToken(String token) {
+        String[] parts = token.split(SEPARATOR);
+        if (parts.length != 2)
+            throw new IllegalArgumentException("Invalid credit card token");
+        return parts[PAYMENT_TOKEN_IDX];
+    }
+
+}
